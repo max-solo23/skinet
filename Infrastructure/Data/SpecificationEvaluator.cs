eace42625a03eb40209935b7e513ce866ae5f9f1
Maksym Solomyanov <maksym.solomyanov@gmail.com>
2025-05-02 21:27:59 +0200

    add Specification pattern support with enhanced ISpecification and BaseSpecification classes

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -24,4 +24,32 @@ public class SpecificationEvaluator<T> where T : BaseEntity
 
         return query;
     }
+
+        public static IQueryable<TResult> GetQuery<TSpec, TResult>(
+            IQueryable<T> query, ISpecification<T, TResult> specification)
+    {
+        if(specification.Criteria != null)
+        {
+            query = query.Where(specification.Criteria);
+        }
+
+        if(specification.OrderBy != null)
+        {
+            query = query.OrderBy(specification.OrderBy);
+        }
+
+        if(specification.OrderByDescending != null)
+        {
+            query = query.OrderByDescending(specification.OrderByDescending);
+        }
+
+        var selectQuery = query as IQueryable<TResult>;
+
+        if(specification.Select != null)
+        {
+            selectQuery = query.Select(specification.Select);
+        }
+
+        return selectQuery ?? query.Cast<TResult>();
+    }
 }
