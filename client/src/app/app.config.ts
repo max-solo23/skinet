43d4bbaae587a66df2ae36292c973f8e142ca699
Maksym Solomyanov <maksym.solomyanov@gmail.com>
2025-05-09 16:26:11 +0200

    implement HTTP call in Angular client to show API data on page

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,12 +3,14 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimations()
+    provideAnimations(),
+    provideHttpClient()
   ]
 };
 
