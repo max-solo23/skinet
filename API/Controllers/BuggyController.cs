9e8e69854e50a111e9c542003bfe1f443d7f3efd
Maksym Solomyanov <maksym.solomyanov@gmail.com>
2025-05-30 18:52:20 +0200

    Add secret endpoint to BuggyController with authorization

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,4 +1,6 @@
+using System.Security.Claims;
 using API.DTOs;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -10,7 +12,7 @@ public class BuggyController : BaseApiController
     {
         return Unauthorized();
     }
-    
+
     [HttpGet("badrequest")]
     public IActionResult GetBadRequest()
     {
@@ -28,4 +30,15 @@ public class BuggyController : BaseApiController
     {
         return Ok();
     }
+
+    [Authorize]
+    [HttpGet("secret")]
+    public IActionResult GetSecret()
+    {
+        var name = User.FindFirst(ClaimTypes.Name)?.Value;
+        var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        return Ok("Hello " + name + " with the id of " + id);
+    }
+    
 }
