368ebb15262e63cb5fb28d257fec3e3e2241de5f
Maksym Solomyanov <maksym.solomyanov@gmail.com>
2025-06-03 18:39:37 +0200

    Add AddressDto and Address mapping extensions; implement CreateOrUpdateAddress method in AccountController

--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -8,10 +8,23 @@ namespace API.Extensions;
 
 public static class ClaimsPrincipleExtensions
 {
-    public static async Task<AppUser> GetUserByEmail(this UserManager<AppUser> userManager, ClaimsPrincipal user)
+    public static async Task<AppUser> GetUserByEmail(this UserManager<AppUser> userManager,
+        ClaimsPrincipal user)
     {
         var userToReturn = await userManager.Users.FirstOrDefaultAsync(x =>
             x.Email == user.GetEmail());
+
+        if (userToReturn == null) throw new AuthenticationException("User not found");
+
+        return userToReturn;
+    }
+
+    public static async Task<AppUser> GetUserByEmailWithAddress(this UserManager<AppUser> userManager,
+        ClaimsPrincipal user)
+    {
+        var userToReturn = await userManager.Users
+            .Include(x => x.Address)
+            .FirstOrDefaultAsync(x => x.Email == user.GetEmail());
 
         if (userToReturn == null) throw new AuthenticationException("User not found");
 
