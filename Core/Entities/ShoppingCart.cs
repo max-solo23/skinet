42656338d343869867f353585cbe72584bdfbf7a
Maksym Solomyanov <maksym.solomyanov@gmail.com>
2025-07-13 18:56:03 +0200

    Implement PaymentService to create or update payment intents and enhance ShoppingCart with payment-related properties

--- Core/Entities/ShoppingCart.cs
+++ Core/Entities/ShoppingCart.cs
@@ -4,4 +4,7 @@ public class ShoppingCart
 {
     public string Id { get; set; }
     public LinkedList<CartItem> Items { get; set; } = [];
+    public int? DeliveryMethodId { get; set; }
+    public string? ClientSecret { get; set; }
+    public string? PaymentIntentId { get; set; }
 }
