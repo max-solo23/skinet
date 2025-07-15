127efcf521e4a794f677dfbfbbac16a782ab0917
Maksym Solomyanov <maksym.solomyanov@gmail.com>
2025-07-15 20:50:10 +0200

    Refactor checkout and order summary components for improved layout and functionality

--- client/src/app/features/checkout/checkout.component.ts
+++ client/src/app/features/checkout/checkout.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatStepperModule } from '@angular/material/stepper';
+import { OrderSummaryComponent } from "../../shared/components/order-summary/order-summary.component";
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [
+    OrderSummaryComponent,
+    MatStepperModule
+  ],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss'
 })
