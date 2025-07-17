a36e2368a5a9b453782ed41488ff9d44cae5eb45
Maksym Solomyanov <maksym.solomyanov@gmail.com>
2025-07-17 18:59:41 +0200

    Add Stripe integration with StripeService and update Cart model for payment handling

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5001/api/'
+    apiUrl: 'https://localhost:5001/api/',
+    stripePublicKey: 'pk_test_51RjPJwGfEbcQnHtFPwmMPBQkxcuIssvsImVi5hMdetZeI9K4e9K6J4IsILHzTcynnj6t7aWTJcJ6dKAVbgG72qJ0000HmqKpBP'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'api/'
+    apiUrl: 'api/',
+    stripePublicKey: 'pk_test_51RjPJwGfEbcQnHtFPwmMPBQkxcuIssvsImVi5hMdetZeI9K4e9K6J4IsILHzTcynnj6t7aWTJcJ6dKAVbgG72qJ0000HmqKpBP'
 };
