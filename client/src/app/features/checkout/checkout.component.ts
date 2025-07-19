4408bb5410e93329a6a838bd1c133d252c892583
Maksym Solomyanov <maksym.solomyanov@gmail.com>
2025-07-19 19:51:59 +0200

    Add address element integration in checkout process and enhance StripeService

--- client/src/app/features/checkout/checkout.component.ts
+++ client/src/app/features/checkout/checkout.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatStepperModule } from '@angular/material/stepper';
 import { OrderSummaryComponent } from "../../shared/components/order-summary/order-summary.component";
+import { MatButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { StripeService } from '../../core/services/stripe.service';
+import { StripeAddressElement } from '@stripe/stripe-js';
+import { SnackbarService } from '../../core/services/snackbar.service';
 
 @Component({
   selector: 'app-checkout',
   imports: [
     OrderSummaryComponent,
-    MatStepperModule
+    MatStepperModule,
+    MatButton,
+    RouterLink
   ],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss'
 })
 export class CheckoutComponent {
+  private stripeService = inject(StripeService);
+  private snackbar = inject(SnackbarService);
+  addressElement?: StripeAddressElement;
 
+  async ngOnInit() {
+    try {
+      this.addressElement = await this.stripeService.createAddressElement();
+      this.addressElement.mount('#address-element');
+    } catch (error: any) {
+      this.snackbar.error(error.message);
+    }
+  }
 }
